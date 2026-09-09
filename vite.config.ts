40a60cc7b0d581ff03a2f9d2519c938abaeee3b3
yuxian.kao <yuxian_kao@genibuilder.com>
2026-09-10 05:52:33 +0800

    set url base to include repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	base: '/<REPO>/',
 	server: {
 		port: 6767,
 		host: true,
