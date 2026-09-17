041d4b8a1f669ab74faf5a1527825633fdaebad3
0xeaeffa <your_email@example.com>
2026-09-17 01:23:39 -0400

    set willReadFrequently in getContext for loadPixels() and updatePixels()

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,3 +7,16 @@ export function mapRange(
 ): number {
   return outMin + ((value - inMin) / (inMax - inMin)) * (outMax - outMin);
 }
+
+// Do this once, before `new p5(...)` runs
+export const originalGetContext = HTMLCanvasElement.prototype.getContext;
+// @ts-ignore - overriding to inject willReadFrequently
+HTMLCanvasElement.prototype.getContext = function (
+  type: string,
+  attributes?: any,
+) {
+  if (type === '2d') {
+    attributes = { ...attributes, willReadFrequently: true };
+  }
+  return originalGetContext.call(this, type, attributes);
+};
